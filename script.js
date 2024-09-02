a874c9a2069c649404c224a75231b24a8868ba4f
Rahul Ji Ara <rahuljiaraofficial@outlook.com>
2024-09-02 22:48:45 +0530

    home & about page Date : 2-sept-2024 Desktop vesrion of ABout Page & Home Page Added Team Images also have added Rahul Ji Ara

---
+++ script.js
@@ -0,0 +1,9 @@
+// About Page
+
+let abouMeContent = document.getElementById("about-app-read-more");
+
+abouMeContent.style.display = "none";
+aboutMeReadMoreFunction = ()=>{
+    abouMeContent.style.display == "none" ? abouMeContent.style.display = "block" : abouMeContent.style.display = "none"
+;
+}
